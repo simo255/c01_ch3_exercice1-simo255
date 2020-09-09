b4752bc10e2db89f424bf34281f82a337300971d
simo255 <bmohamed255@gmail.com>
2020-09-09 19:49:13 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -6,10 +6,6 @@ def square_root(number: int) -> float:
     # TODO completer la fonction
     return square_rooted
 import math
-pow=input('veuillez entrer un nombre dont vous voulez calculer le carré : ')
-r= math.pow(int(pow), 2)
-print('le resultat est', r)
-
 sqrt=input('veuillez entrer un nombre dont vous voulez calculer la racine carrée : ')
 s= math.sqrt(int(sqrt))
 print('le resulteat est', s)
